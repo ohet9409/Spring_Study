cf578a0054e7204bb5f9f638a95ad0679032ebbc
oet <dhdmsxor1@naver.com>
2020-03-04 21:06:46 +0900

    108p 로그양 줄이는게 해결 못함

--- jex00/src/main/java/com/spring/config/RootConfig.java
+++ jex00/src/main/java/com/spring/config/RootConfig.java
@@ -1,10 +1,42 @@
 package com.spring.config;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 // 설정파일을 대신하는 클래스
 @Configuration
 @ComponentScan(basePackages = {"com.spring"})
+// mapper 인터페이스 사용
+@MapperScan(basePackages = {"com.spring.mapper"})
 public class RootConfig {
-
+	
+	@Bean
+	public DataSource dataSource() {
+		
+		HikariConfig hikariConfig = new HikariConfig();
+		//hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		//hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:XE");
+		hikariConfig.setUsername("book_ex");
+		hikariConfig.setPassword("book_ex");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		return dataSource;
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception{
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		return (SqlSessionFactory)sqlSessionFactory.getObject();
+	}
 }
