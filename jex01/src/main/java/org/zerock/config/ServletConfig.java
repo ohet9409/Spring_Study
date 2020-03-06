12075bdfdb2a4c297d47417a313b3c9fcf2b6a9a
oet <dhdmsxor1@naver.com>
2020-03-06 22:46:41 +0900

    p125 ex01, jex01 생성

---
+++ jex01/src/main/java/org/zerock/config/ServletConfig.java
@@ -0,0 +1,30 @@
+package org.zerock.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+//어노테이션 기반의 SpringMvc를 구성할때 필요한 bean설정들을 자동으로 해주는 어노테이션
+@EnableWebMvc
+@ComponentScan(basePackages = {"org.zerock.controller"})
+public class ServletConfig implements WebMvcConfigurer{
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/WEB-INF/views/");
+		bean.setSuffix(".jsp");
+		registry.viewResolver(bean);
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+}
